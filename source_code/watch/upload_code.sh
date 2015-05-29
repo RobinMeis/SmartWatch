e0fd64f94737f7ef9b2acfa5fffdab29e80f2913
Robin Meis <hc.devel@yahoo.de>
2015-05-29 21:26:04 +0200

    Updated for external libraries
    Compiler now links header files correctly

--- source_code/watch/upload_code.sh
+++ source_code/watch/upload_code.sh
@@ -1,7 +1,8 @@
 echo Es sind root Rechte erforderlich!
 sudo echo OK: Root Rechte erhalten!
-avr-gcc -mmcu=atmega328p -Os -c main.c -o main.o
-avr-gcc main.o -o main.elf
+rm program main.elf main.hex
+avr-gcc -mmcu=atmega328p -Os -o program *.c
+avr-gcc program -o main.elf
 avr-objcopy -O ihex -j .text -j .data main.elf main.hex
 avr-size --mcu=atmega328p -C main.elf
 sudo avrdude -c avrisp2 -p m328p -U flash:w:main.hex:a
