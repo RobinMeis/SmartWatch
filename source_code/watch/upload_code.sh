cfbab611d6e0cbc2c87a6ed756e8416c5c77f3aa
Robin Meis <hc.devel@yahoo.de>
2015-05-29 21:28:17 +0200

    Bug fix
    Corrected error which caused gitignore to fail on ignoring binary

--- source_code/watch/upload_code.sh
+++ source_code/watch/upload_code.sh
@@ -1,8 +1,8 @@
 echo Es sind root Rechte erforderlich!
 sudo echo OK: Root Rechte erhalten!
-rm program main.elf main.hex
-avr-gcc -mmcu=atmega328p -Os -o program *.c
-avr-gcc program -o main.elf
+rm program.o main.elf main.hex
+avr-gcc -mmcu=atmega328p -Os -o program.o *.c
+avr-gcc program.o -o main.elf
 avr-objcopy -O ihex -j .text -j .data main.elf main.hex
 avr-size --mcu=atmega328p -C main.elf
 sudo avrdude -c avrisp2 -p m328p -U flash:w:main.hex:a
