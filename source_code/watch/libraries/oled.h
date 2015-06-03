68cacdc550ad4f5b2824e317c562d0d884f2d8e1
Robin Meis <hc.devel@yahoo.de>
2015-06-03 22:36:19 +0200

    Cleaned up oled library
    OLED library has now all required functions included

--- source_code/watch/libraries/oled.h
+++ source_code/watch/libraries/oled.h
@@ -5,4 +5,9 @@ void OLED_Start(void);
 void OLED_Stop(void);
 void OLED_Write(uint8_t data);
 void OLED_Command(uint8_t command);
+void OLED_on(void);
+void OLED_off(void);
+void OLED_set_pixel(unsigned char x, unsigned char y, char state);
+void OLED_display(void);
+void OLED_clear(void);
 #endif
