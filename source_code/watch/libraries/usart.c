68aca1c48f53717b5edf0d8fc8d335bbe7b2102f
Robin Meis <hc.devel@yahoo.de>
2015-06-04 22:54:02 +0200

    Switched to interrupts
    UART is now interrupt based

--- source_code/watch/libraries/usart.c
+++ source_code/watch/libraries/usart.c
@@ -1,17 +1,47 @@
 #define F_CPU 8000000UL
 #define BAUD 9600
 #define BAUD_PRESCALLER (((F_CPU / (BAUD * 16UL))) - 1)
+#define BUFFER_SIZE 20
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile unsigned char buffer[BUFFER_SIZE], buffer_index=0;
 
 void usart_init(void) {
   UBRR0H = (uint8_t)(BAUD_PRESCALLER>>8);
   UBRR0L = (uint8_t)(BAUD_PRESCALLER);
   UCSR0B = (1<<RXEN0)|(1<<TXEN0);
   UCSR0C = ((1<<UCSZ00)|(1<<UCSZ01));
+
+  UCSR0B |= (1<<RXEN0)|(1<<RXCIE0);
 }
 
-void usart_send_char(unsigned char data) {
+ISR(USART_RX_vect) { //Receive interrupt
+  unsigned char data=UDR0;
+  ++buffer_index;
+  if (buffer_index==BUFFER_SIZE||data=='+') {
+    UCSR0B &= ~((1<<RXEN0)|(1<<RXCIE0)); //Switch to transmission interruot
+    UCSR0B |= (1<<TXEN0)|(1<<UDRIE0);
+    return;
+  }
+  buffer[buffer_index] = data;
+}
+
+ISR(USART_UDRE_vect) { //Transmission interrupt
+  UDR0=buffer[buffer_index];
+  --buffer_index;
+  if (buffer_index==0) {
+    UCSR0B &= ~((1<<TXEN0)|(1<<UDRIE0)); //Switch to receive interrupt
+    UCSR0B |= (1<<RXEN0)|(1<<RXCIE0);
+  }
+}
+
+void parse_buffer(void) {
+
+}
+
+/*void usart_send_char(unsigned char data) { //Not required anymore since UART is now based on interrupts TODO: Remove when interrupt tested
   while(!(UCSR0A & (1<<UDRE0)));
   UDR0 = data;
 }
@@ -24,4 +54,4 @@ void usart_send_string(char* string) {
 unsigned char usart_receive(void) {
   while(!(UCSR0A & (1<<RXC0)));
   return UDR0;
-}
+}*/
--- source_code/watch/main.c
+++ source_code/watch/main.c
@@ -1,21 +1,25 @@
 //TODO: Do every TODO in every file!
+//TODO: Check each header file for missing functions
 
 #define F_CPU 8000000UL //TODO: Set Global
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #include "libraries/usart.h"
 #include "libraries/oled.h"
 #include "libraries/font.h"
 
 int main(void){
-  usart_init();
-  usart_send_string("ready\n");
-
   OLED_Init();
   OLED_display(); //Boot screen
-  _delay_ms(500);
+  usart_init();
+  sei();
+  //usart_send_string("ready\n");
+
+
+  //_delay_ms(500);
   OLED_clear();
   OLED_display();
 
@@ -25,8 +29,12 @@ int main(void){
   draw_rectangle(160,10,10,10,1);
   OLED_display();
 
-  usart_send_string("done\n");
+  //usart_send_string("done\n");
   while (1) {
+    //usart_send_char(usart_receive());
+    //usart_send_string("ding\n");
+    //_delay_ms(100000);
+    parse_buffer();
   }
   return 0;
 }
