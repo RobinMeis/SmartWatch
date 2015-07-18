7372b5f157e6e1de263219029f67699bd42b392c
Robin Meis <hc.devel@yahoo.de>
2015-07-18 22:15:02 +0200

    Increased upload speed

--- source_code/watch/upload_code.sh
+++ source_code/watch/upload_code.sh
@@ -5,4 +5,4 @@ avr-gcc -mmcu=atmega328p -Os -o program.o *.c libraries/*.c apps/*.c
 avr-gcc program.o -o main.elf
 avr-objcopy -O ihex -j .text -j .data main.elf main.hex
 avr-size --mcu=atmega328p -C main.elf
-sudo avrdude -c avrisp2 -p m328p -U flash:w:main.hex:a
+sudo avrdude -B 1 -c avrisp2 -p m328p -U flash:w:main.hex:a
