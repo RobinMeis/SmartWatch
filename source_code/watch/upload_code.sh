4614a2fd9d54aae9d61a56095faa0836ba57ff9a
Robin Meis <hc.devel@yahoo.de>
2015-07-22 12:06:38 +0200

    .text full
    Created error, even when controller wasn't full

--- source_code/watch/upload_code.sh
+++ source_code/watch/upload_code.sh
@@ -1,8 +1,7 @@
 echo Es sind root Rechte erforderlich!
 sudo echo OK: Root Rechte erhalten!
 rm program.o main.elf main.hex
-avr-gcc -mmcu=atmega328p -Os -o program.o *.c libraries/*.c apps/*.c
-avr-gcc program.o -o main.elf
+avr-gcc -mmcu=atmega328p -Os -o main.elf *.c libraries/*.c apps/*/*.c
 avr-objcopy -O ihex -j .text -j .data main.elf main.hex
 avr-size --mcu=atmega328p -C main.elf
 sudo avrdude -B 1 -c avrisp2 -p m328p -U flash:w:main.hex:a
